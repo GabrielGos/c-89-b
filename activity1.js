f962f9f0f0d4780d6ccefa314bbea7836466cadd
GabrielGos <131292243+GabrielGos@users.noreply.github.com>
2023-10-25 14:50:17 -0300

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,23 @@
+var score = 0;
+
+
+function updateScore()
+{
+    score = score + 1;
+    document.getElementById("score").innerHTML = "pontuação : " + score;
+
+}
+
+
+function saveScore()
+{
+    localStorage.setItem("score", score);
+}
+
+function nextPage()
+{ 
+    function nextPage(){
+        window.location = "activity_2.html";
+    }
+}    
+
